7db7b9dd59a3aea9def7445c4968f85972c04e73
Vinicius <vini_532cursos@hotmail.com>
2024-12-24 02:51:16 -0300

    feat: Criado um endpoint novo para o retorno do html.

--- fast_api_do_zero/app.py
+++ fast_api_do_zero/app.py
@@ -1,4 +1,7 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +9,16 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'Primeira vez subindo no fastApi'}
+
+
+@app.get('/resposta', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def retorno_html():
+    return """
+    <html>
+        <head>
+            <title> Nosso olá mundo!</title>
+        </head>
+        <body>
+            <h1>Olá Mundo </h1>
+        </body>
+        </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,23 @@ def test_root_deve_retornar_ok_e_primeira_vez_subindo_no_fastAPi():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Primeira vez subindo no fastApi'}
+
+
+def test_resposta_deve_retornar_ok_em_hmtl_e_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/resposta')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+        <head>
+            <title> Nosso olá mundo!</title>
+        </head>
+        <body>
+            <h1>Olá Mundo </h1>
+        </body>
+        </html>"""
+    )
