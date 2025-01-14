73017832480a11a8a2797be357669fd75e9d1c72
ViFernaandes <vini_532cursos@hotmail.com>
2025-01-13 22:27:38 -0300

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

---
+++ tests/test_db.py
@@ -0,0 +1,18 @@
+from sqlalchemy import select
+
+from fast_api_do_zero.models import User
+
+
+def test_create_user(session):
+    user = User(
+        username='test',
+        email='test@test.com',
+        password='secret',
+    )
+
+    session.add(user)
+    session.commit()
+
+    result = session.scalar(select(User).where(User.email == 'test@test.com'))
+
+    assert result.username == 'test'
